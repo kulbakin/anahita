4d163f96af4f845ef55a994cede279d6ef36346a
Pavel Kulbakin <p.kulbakin@gmail.com>
2013-12-13 20:52:36 +0200

    fix: javascript error on profile page [...]
    The error appears when there is no component specific add forms (aka
    composer forms) defined, i.e. only notes can be added from profile page.
    It caused by the fact that in such a case element containing
    ComposerTabs is replaced with empty content and subsequent calls of
    el.getParent() return null. The fix requests for container element
    during initialization and stores result in variable allowing referencing
    to it even after initializing tag is removed.

--- src/media/com_composer/js/composer.js
+++ src/media/com_composer/js/composer.js
@@ -1,104 +1,94 @@
 Behavior.addGlobalFilter('ComposerForm', {
-    defaults : {
-      trigger : 'button'  
+    defaults: {
+        trigger : 'button'
     },
-    setup : function(el,api)
-    {        
-        if ( el.getElement('input[type="file"]') )
-        {
-            (function(el){
+    setup: function (el, api) {
+        if (el.getElement('input[type="file"]')) {
+            (function (el) {
                 var submitted = false;
                 var action = el.get('action').toURI();
-                action.setData({'format':'raw'});
+                action.setData({'format': 'raw'});
                 el.set('action', action);
-                new Element('input',{type:'hidden','name':'composed','value':'true'}).inject(el);
-                new Element('input',{type:'hidden','name':'format','value':'raw'}).inject(el);
+                new Element('input', {type: 'hidden', 'name': 'composed', 'value': 'true'}).inject(el);
+                new Element('input', {type: 'hidden', 'name': 'format', 'value': 'raw'}).inject(el);
                 var iframe = new IFrame().hide().inject(el);
                 el.set('target', iframe.get('name'));
-                iframe.addEvent('load', function(){
+                iframe.addEvent('load', function () {
                     el.unspin();
-                    if ( submitted ) {
-                        var story   = window.frames[this.get('name')].document.body.innerHTML;
+                    if (submitted) {
+                        var story = window.frames[this.get('name')].document.body.innerHTML;
                         story = story.parseHTML().getElement('*');
-                        story.inject(document.id('an-stories'), 'top');                        
+                        story.inject(document.id('an-stories'), 'top');
                         el.reset();
-				        window.delegator.attach(document);
-				        window.behavior.apply(document.body);
+                        window.delegator.attach(document);
+                        window.behavior.apply(document.body);
                     }
                 });
-                //create an iframe 
-                el.addEvent('submit', function(e){
+                // create an iframe
+                el.addEvent('submit', function (e) {
                     submitted = true;
-                    if ( el.retrieve('validator').validate() )
+                    if (el.retrieve('validator').validate()) {
                         el.spin();
+                    }
                 });
-                
             })(el);
-        }
-        else 
-        {
+        } else {
             var trigger = el.getElement(api.getAs(String, 'trigger'));
-            trigger.addEvent('click', function(event){            
+            trigger.addEvent('click', function (event) {
                 event.stop();
                 el.ajaxRequest({
-                    data            : el.toQueryString() + '&composed=true',
-                    inject          : 'an-stories',
-                    onSuccess       : function() {
+                    data: el.toQueryString() + '&composed=true',
+                    inject: 'an-stories',
+                    onSuccess: function () {
                         this.reset();
                     }.bind(el)
-                }).send();            
+                }).send();
             });
         }
     }
 });
 
 (function(){
-    var composerTabs;    
-    Behavior.addGlobalFilter('ComposerTabs', {    
-        defaults : {
-            focus : 'input'
+    var composerTabs;
+    Behavior.addGlobalFilter('ComposerTabs', {
+        defaults: {
+            focus: 'input'
         },
-        setup : function(el,api)
-        {
-            var focusInput = function(section) {
-              //element.getElement(api.getAs(String,'focus')).focus();
-                var element;
-                if ( element = section.getElement('input[type="text"]') ) {
-                    element.focus();
-                } else if ( element = section.getElement('textarea') ) {
+        setup: function (el, api) {
+            var container = el.getParent();
+            var focusInput = function (section) {
+                var element = section.getElement('input[type="text"]') || section.getElement('textarea');
+                if (element) {
                     element.focus();
                 }
-            }
+            };
+            
             window.behavior.applyFilter(el.getParent(), Behavior.getFilter('BS.Dropdown'));
             window.behavior.applyFilter(el, Behavior.getFilter('BS.Tabs'));
             var nagivation  = document.getElement('.sidelinks');
             composerTabs    = el.getBehaviorResult('BS.Tabs');
-            //if there's a navigation
-            if ( nagivation )
-            {
-                window.behavior.applyFilter(nagivation, Behavior.getFilter('BS.Tabs'));                                  
-                var profileTabs = nagivation.getBehaviorResult('BS.Tabs')
-                if ( profileTabs ) 
-                {
-                    profileTabs.addEvent('active', function(idx, nav, section){
-                        if ( idx == 0 ) {
-                            el.getParent().show();
+            // if there's a navigation
+            if (nagivation) {
+                window.behavior.applyFilter(nagivation, Behavior.getFilter('BS.Tabs'));
+                var profileTabs = nagivation.getBehaviorResult('BS.Tabs');
+                if (profileTabs) {
+                    profileTabs.addEvent('active', function (idx, nav, section) {
+                        if (idx == 0) {
+                            container.show();
                         } else {
-                            el.getParent().hide();
-                        }                
+                            container.hide();
+                        }
                     });
-                }                
+                }
             }
-          
+            
             composerTabs.addEvent('active', function(idx,section,tab) {
-				el.getElement('.composer-button-title').set('text', tab.get('text'));
-                if ( section.get('data-content-url') )
-                {
-                    if ( !section.retrieve('request'))
-                    {
+                el.getElement('.composer-button-title').set('text', tab.get('text'));
+                if (section.get('data-content-url')) {
+                    if ( ! section.retrieve('request')) {
                         var request = section.ajaxRequest({
-                            url         : section.get('data-content-url'),
-                            onSuccess   : function() {
+                            url: section.get('data-content-url'),
+                            onSuccess: function () {
                                 var element = this.response.text.parseHTML().getElement('*');
                                 section.setContent(element);
                                 element.get('tween').chain(function(){
@@ -111,27 +101,23 @@ Behavior.addGlobalFilter('ComposerForm', {
                         section.store('request', request);
                     }
                  }
-                               
              });
-            composerTabs.addEvent('background', function(idx,section,tab){
-                if ( section.getElement('form') && section.getElement('form').get('validator') ) {
+            composerTabs.addEvent('background', function (idx,section,tab) {
+                if (section.getElement('form') && section.getElement('form').get('validator')) {
                     section.getElement('form').get('validator').reset();
                 }
-            });     
-                        
-            if ( composerTabs.tabs.length == 1 ) {
+            });
+            
+            if (composerTabs.tabs.length == 1 ) {
                 composerTabs.tabs[0].retrieve('section').replaces(el);
             }
-                        
         }
-    
     });
-    Delegator.register('click', 'LoadComposerTab', function(event, el, api) {        
-    	var index = api.getAs(Number, 'index');        
-    	if ( composerTabs && composerTabs.now == index )
-        {
+    Delegator.register('click', 'LoadComposerTab', function (event, el, api) {
+        var index = api.getAs(Number, 'index');
+        if (composerTabs && composerTabs.now == index) {
             composerTabs.now = -1;
-            composerTabs.show(index);                
+            composerTabs.show(index);
         }
-    });    
+    });
 })();
