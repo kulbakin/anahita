1d6ad19e722d19d2c7c6527b696809644960b65e
Pavel Kulbakin <p.kulbakin@gmail.com>
2013-12-16 01:56:36 +0200

    upd: add method to notify admins

--- src/site/components/com_notifications/controllers/behaviors/notifier.php
+++ src/site/components/com_notifications/controllers/behaviors/notifier.php
@@ -1,52 +1,58 @@
 <?php
-
-/** 
- * LICENSE: ##LICENSE##
+/**
+ * Publisher Behavior. Publishes stories after an action
  * 
  * @category   Anahita
  * @package    Com_Notifications
  * @subpackage Controller_Behavior
+ * @author     Pavel Kulbakin <p.kulbakin@gmail.com>
  * @author     Arash Sanieyan <ash@anahitapolis.com>
  * @author     Rastin Mehr <rastin@anahitapolis.com>
- * @copyright  2008 - 2011 rmdStudio Inc./Peerglobe Technology Inc
- * @license    GNU GPLv3 <http://www.gnu.org/licenses/gpl-3.0.html>
- * @version    SVN: $Id: resource.php 11985 2012-01-12 10:53:20Z asanieyan $
- * @link       http://www.anahitapolis.com
- */
-
-/**
- * Publisher Behavior. Publishes stories after an action
- *
- * @category   Anahita
- * @package    Com_Notifications
- * @subpackage Controller_Behavior
- * @author     Arash Sanieyan <ash@anahitapolis.com>
- * @author     Rastin Mehr <rastin@anahitapolis.com>
- * @license    GNU GPLv3 <http://www.gnu.org/licenses/gpl-3.0.html>
- * @link       http://www.anahitapolis.com
+ * @license    GNU GPLv3
  */
 class ComNotificationsControllerBehaviorNotifier extends KControllerBehaviorAbstract
 {
     /**
      * Creates a notification
-     *
+     * 
      * @param array $data Notification data
-     *
      * @return ComNotificationDomainEntityNotification
      */
     public function createNotification($data = array())
-    {        
+    {
         $data = new KConfig($data);
-    
+        
         $data->append(array(
-                'component' => 'com_'.$this->_mixer->getIdentifier()->package,
-                'subject'	=> get_viewer()
+            'component' => 'com_'.$this->_mixer->getIdentifier()->package,
+            'subject'   => get_viewer(),
         ));
-            
-        $notification = $this->getService('repos:notifications.notification')->getEntity(array('data'=>$data));
-    
-        $notification->removeSubscribers( get_viewer() );
-    
+        
+        $notification = $this->getService('repos:notifications.notification')->getEntity(array('data' => $data));
+        $notification->removeSubscribers(get_viewer());
+        
         return $notification;
-    } 
+    }
+    
+    /**
+     * Created a notification sent to administrators
+     * 
+     * @param array $data Notification data
+     * @return ComNotificationDomainEntityNotification
+     */
+    public function createAdminNotification($data = array())
+    {
+        $data = new KConfig($data);
+        
+        $adminIds = $this->getService('repos:people.person')->getQuery()
+            ->where('userType', 'IN', array('Administrator', 'Super Administrator'))
+            ->fetchValues('id');
+        
+        if ($adminIds) {
+            return $this->createNotification($data->append(array(
+                'subscribers' => $adminIds,
+            )));
+        }
+        
+        return null;
+    }
 }
