83693a49317215f39a2593de619f71824869cc39
Pavel Kulbakin <p.kulbakin@gmail.com>
2013-12-22 21:13:01 +0200

    fix: PlgStorageAbstract::_write [...]
    remove unused $public parameter from protected _write method

--- src/plugins/storage/local.php
+++ src/plugins/storage/local.php
@@ -1,55 +1,41 @@
 <?php
-
-/** 
- * LICENSE: ##LICENSE##
- * 
- * @category   Anahita
- * @package    Plg_Storage
- * @author     Arash Sanieyan <ash@anahitapolis.com>
- * @author     Rastin Mehr <rastin@anahitapolis.com>
- * @copyright  2008 - 2010 rmdStudio Inc./Peerglobe Technology Inc
- * @license    GNU GPLv3 <http://www.gnu.org/licenses/gpl-3.0.html>
- * @version    SVN: $Id$
- * @link       http://www.anahitapolis.com
- */
-
 /**
- * Local storage plugin. 
+ * Local storage plugin.
  * 
  * @category   Anahita
  * @package    Plg_Storage
  * @author     Arash Sanieyan <ash@anahitapolis.com>
  * @author     Rastin Mehr <rastin@anahitapolis.com>
  * @license    GNU GPLv3 <http://www.gnu.org/licenses/gpl-3.0.html>
- * @link       http://www.anahitapolis.com
+ * @copyright  2008 - 2010 rmdStudio Inc./Peerglobe Technology Inc
  */
 class PlgStorageLocal extends PlgStorageAbstract
 {
     /**
      * Initializes the default configuration for the object
-     *
+     * 
      * Called from {@link __construct()} as a first step of object instantiation.
-     *
+     * 
      * @param KConfig $config An optional KConfig object with configuration options.
-     *
      * @return void
      */
     protected function _initialize(KConfig $config)
     {
         $base_uri  = JURI::base();
         
-        if ( JFactory::getApplication()->getClientId() != 0 )
+        if (JFactory::getApplication()->getClientId() != 0) {
             $base_uri = preg_replace('/\w+\/$/', '', $base_uri);
+        }
         
         $config->append(array(
-              'folder'     => 'assets',
-              'base_uri'   => $base_uri,
-               'root'      => JPATH_ROOT
+            'folder'   => 'assets',
+            'base_uri' => $base_uri,
+            'root'     => JPATH_ROOT
         ));
-    
+        
         parent::_initialize($config);
     }
-        
+    
     /**
      * {@inheritdoc}
      */
@@ -62,23 +48,21 @@ class PlgStorageLocal extends PlgStorageAbstract
     /**
      * {@inheritdoc}
      */
-    protected function _write($path, $data, $public)
+    protected function _write($path, $data)
     {
         $path = $this->_realpath($path);
-        
         $dir  = dirname($path);
         
-        if ( !file_exists($dir) )
-        {
+        if ( ! file_exists($dir)) {
             mkdir($dir, 0707, true);
         }
         
-        return file_put_contents($path, (string) $data);
+        return file_put_contents($path, (string)$data);
     }
     
     /**
      * {@inheritdoc}
-     */    
+     */
     protected function _exists($path)
     {
         $path = $this->_realpath($path);
@@ -87,17 +71,14 @@ class PlgStorageLocal extends PlgStorageAbstract
     
     /**
      * {@inheritdoc}
-     */    
+     */
     protected function _delete($path)
     {
         $path = $this->_realpath($path);
         jimport('joomla.filesystem.folder');
-        if ( is_dir($path) )
-        {
+        if (is_dir($path)) {
             JFolder::delete($path);
-        }            
-        elseif ( file_exists($path) ) 
-        {
+        } elseif (file_exists($path)) {
             @unlink($path);
         }
     }
@@ -112,13 +93,12 @@ class PlgStorageLocal extends PlgStorageAbstract
     
     /**
      * Return the realpath path of a relative path 
-     *
-     * @param string $path The path to append
      * 
+     * @param string $path The path to append
      * @return
      */
     protected function _realpath($relative)
     {
         return $this->_params->root.DS.str_replace('/', DS, $relative);
-    }    
+    }
 }
--- src/plugins/storage/s3.php
+++ src/plugins/storage/s3.php
@@ -1,17 +1,4 @@
 <?php
-
-/** 
- * LICENSE: ##LICENSE##
- * 
- * @category   Anahita
- * @package    Plg_Storage
- * @author     Arash Sanieyan <ash@anahitapolis.com>
- * @author     Rastin Mehr <rastin@anahitapolis.com>
- * @copyright  2008 - 2010 rmdStudio Inc./Peerglobe Technology Inc
- * @license    GNU GPLv3 <http://www.gnu.org/licenses/gpl-3.0.html>
- * @version    SVN: $Id$
- * @link       http://www.anahitapolis.com
- */
 require_once 's3lib.php';
 
 /**
@@ -22,24 +9,23 @@ require_once 's3lib.php';
  * @author     Arash Sanieyan <ash@anahitapolis.com>
  * @author     Rastin Mehr <rastin@anahitapolis.com>
  * @license    GNU GPLv3 <http://www.gnu.org/licenses/gpl-3.0.html>
- * @link       http://www.anahitapolis.com
+ * @copyright  2008 - 2010 rmdStudio Inc./Peerglobe Technology Inc
  */
 class PlgStorageS3 extends PlgStorageAbstract
-{   
+{
     /**
      * S3 storage
      * 
      * @var S3
-     */ 
+     */
     protected $_s3;
     
     /**
      * Initializes the default configuration for the object
-     *
+     * 
      * Called from {@link __construct()} as a first step of object instantiation.
-     *
+     * 
      * @param KConfig $config An optional KConfig object with configuration options.
-     *
      * @return void
      */
     protected function _initialize(KConfig $config)
@@ -56,7 +42,7 @@ class PlgStorageS3 extends PlgStorageAbstract
         
         $this->_s3 = new S3($config->access_key, $config->secret_key, $config->use_ssl);
     }
-        
+    
     /**
      * {@inheritdoc}
      */
@@ -67,8 +53,8 @@ class PlgStorageS3 extends PlgStorageAbstract
     
     /**
      * {@inheritdoc}
-     */    
-    protected function _write($path, $data, $public)
+     */
+    protected function _write($path, $data)
     {
         $acl	 = $public ? S3::ACL_PUBLIC_READ : S3::ACL_PRIVATE;
         $options = array('Content-Type'=>$this->_s3->__getMimeType($path));
@@ -77,7 +63,7 @@ class PlgStorageS3 extends PlgStorageAbstract
     
     /**
      * {@inheritdoc}
-     */    
+     */
     protected function _exists($path)
     {
         return false;
@@ -85,28 +71,26 @@ class PlgStorageS3 extends PlgStorageAbstract
     
     /**
      * {@inheritdoc}
-     */    
+     */
     protected function _delete($path)
     {
         $paths = $this->_s3->getBucket($this->_params->bucket, $path);
-    
-        foreach($paths as $path) {
+        
+        foreach ($paths as $path) {
             $this->_s3->deleteObject($this->_params->bucket, $path['name']);
         }
     }
     
     /**
      * {@inheritdoc}
-     */    
+     */
     protected function _getUrl($path)
     {
-        if (isset($_SERVER['HTTPS']) && !empty($_SERVER['HTTPS']) && (strtolower($_SERVER['HTTPS']) != 'off'))
-        {
+        if (isset($_SERVER['HTTPS']) && ! empty($_SERVER['HTTPS']) && (strtolower($_SERVER['HTTPS']) != 'off')) {
             $url = 'https://s3.amazonaws.com/'.$this->_params->bucket.'/'.$path;
-        } else
-        {
+        } else {
             $url = 'http://'.$this->_params->bucket.'.s3.amazonaws.com/'.$path;
         }
         return $url;
-    }    
+    }
 }
