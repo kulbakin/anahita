6a52ac71e223149b5f673fa824435bd2ba369242
Pavel Kulbakin <p.kulbakin@gmail.com>
2014-01-03 17:18:24 +0200

    fix: make KDate::format() support %l and %e on Windows

--- packages/Pages/src/site/components/com_pages/views/revision/html/default.php
+++ packages/Pages/src/site/components/com_pages/views/revision/html/default.php
@@ -1,9 +1,9 @@
-<?php defined('KOOWA') or die; ?>
+<?php defined('KOOWA') or die ?>
 
 <module position="sidebar-b" title="<?= @text('COM-PAGES-META-ADDITIONAL-INFORMATION') ?>">
 <ul class="an-meta">
-	<li><span class="label label-info"><?= sprintf(@text('COM-PAGES-PAGE-REVISION-META-NUMBER'), $revision->revisionNum) ?></span></li>
-	<li><?= sprintf( @text('LIB-AN-MEDIUM-AUTHOR'), @date($revision->creationTime, '%B %d %Y - %l:%M %p'), @name($revision->author)) ?></li>
+    <li><span class="label label-info"><?= sprintf(@text('COM-PAGES-PAGE-REVISION-META-NUMBER'), $revision->revisionNum) ?></span></li>
+    <li><?= sprintf( @text('LIB-AN-MEDIUM-AUTHOR'), @date($revision->creationTime, '%B %d %Y - %l:%M %p'), @name($revision->author)) ?></li>
 </ul>
 </module>
 
@@ -11,7 +11,7 @@
 <?= @helper('ui.gadget', LibBaseTemplateObject::getInstance('revisions', array(
     'title' => @text('COM-PAGES-PAGE-REVISIONS'),
     'url'   => 'view=revisions&layout=gadget&pid='.$revision->page->id.'&oid='.$actor->id
-))); ?>
+))) ?>
 </module>
 
 <?= @template('revision') ?>
--- src/site/components/com_notifications/views/notifications/html/list.php
+++ src/site/components/com_notifications/views/notifications/html/list.php
@@ -1,40 +1,36 @@
 <?php defined('KOOWA') or die ?>
-<?php
-$dates = @helper('notifications.group', $notifications);
-?>
-<?php foreach($dates as $date => $notifications) : ?>
-<h3><?=$date?></h3>
-<div id="com-notifications-list-" class="an-entities">
-    <?php foreach($notifications as $notification) : ?>
-    <?php 
-    $data = @helper('parser.parse', $notification, $actor);
-    ?>
-    <div class="an-entity an-record an-removable">
-    	<div class="clearfix">
-            <?php if ($notification->subject): ?>
-	    	<div class="entity-portrait-square">
-	    		<?= @avatar($notification->subject) ?>
-	    	</div>
-            <?php endif ?>
-	    	
-	    	<div class="entity-container">	
-	    		<p class="entity-title">	    			
-        			<?= $data['title']?>
-	    		</p>
-	    	    <div class="body">
-	    	        <?= $data['body']?>
-	    	    </div>
-	    		<div class="entity-meta">
-                	<?= $notification->creationTime->format('%l:%M %p')?>
-        		</div>
-	    	</div>
-    	</div>
+
+<?php $dates = @helper('notifications.group', $notifications) ?>
+<?php foreach ($dates as $date => $notifications): ?>
+    <h3><?=$date?></h3>
+    <div id="com-notifications-list-" class="an-entities">
+        <?php foreach($notifications as $notification): ?>
+            <?php $data = @helper('parser.parse', $notification, $actor) ?>
+            <div class="an-entity an-record an-removable">
+                <div class="clearfix">
+                    <?php if ($notification->subject): ?>
+                        <div class="entity-portrait-square">
+                            <?= @avatar($notification->subject) ?>
+                        </div>
+                    <?php endif ?>
+                    
+                    <div class="entity-container">
+                        <p class="entity-title">
+                            <?= $data['title']?>
+                        </p>
+                        <div class="body">
+                            <?= $data['body']?>
+                        </div>
+                        <div class="entity-meta">
+                            <?= $notification->creationTime->format('%l:%M %p')?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <?php endforeach ?>
     </div>
-    <?php endforeach;?>
-</div>
+<?php endforeach ?>
 
-<?php endforeach; ?>
-
-<?php if (count($dates) == 0) : ?>
-<?= @message(@text('COM-NOTIFICATIONS-EMPTY-LIST-MESSAGE')) ?>
-<?php endif; ?>
+<?php if (0 == count($dates)): ?>
+    <?= @message(@text('COM-NOTIFICATIONS-EMPTY-LIST-MESSAGE')) ?>
+<?php endif ?>
