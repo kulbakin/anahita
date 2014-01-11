3259319620622f8c40da4acecb3c91c571e4c125
Pavel Kulbakin <p.kulbakin@gmail.com>
2014-01-11 21:06:59 +0200

    cleanup

--- packages/Opensocial/src/plugins/user/opensocial.php
+++ packages/Opensocial/src/plugins/user/opensocial.php
@@ -1,38 +1,30 @@
-<?php 
-/**
- * @version		1.0.3
- * @category	Anahita Social Engine™
- * @copyright	Copyright (C) 2008 - 2010 rmdStudio Inc. and Peerglobe Technology Inc. All rights reserved.
- * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl-3.0.html>
- * @link     	http://www.anahitapolis.com
- */
-
-defined('_JEXEC') or die();
+<?php defined('_JEXEC') or die();
 
 jimport('joomla.plugin.plugin');
 
 /**
  * Opensocial user plugin
- *
- * @author		Rastin Mehr  <info@rmdstudio.com>
- * @package		Joomla
- * @subpackage	Anahita
- * @since 		1.5
+ * 
+ * @author      Rastin Mehr  <info@rmdstudio.com>
+ * @package     Joomla
+ * @subpackage  Anahita
+ * @copyright   Copyright (C) 2008 - 2010 rmdStudio Inc. and Peerglobe Technology Inc. All rights reserved.
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl-3.0.html>
  */
 class PlgUserOpensocial extends JPlugin 
 {
     /**
      * store user method
-     *
+     * 
      * Method is called before user data is deleted from the database
-     *
-     * @param 	array		holds the user data
+     * 
+     * @param array holds the user data
      */
     public function onAfterDeleteUser($user)
     {
         $actor_profile = KService::get('repos:opensocial.profile')
             ->find(array('person.userId'=>$use['id']));
-        if ( $actor_profile ) {
+        if ($actor_profile) {
             $actor_profile->delete()->save();
         }
     }
