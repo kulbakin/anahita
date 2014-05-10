a5f38a17b554d204e10874672b30b225e74ccab7
Pavel Kulbakin <p.kulbakin@gmail.com>
2014-05-10 15:07:40 +0400

    fix: do not show toolbar wrapper if command list is empty

--- src/site/components/com_base/templates/helpers/ui/_ui_commands.php
+++ src/site/components/com_base/templates/helpers/ui/_ui_commands.php
@@ -1,8 +1,9 @@
 <?php defined('KOOWA') or die ?>
-<?php if (!empty($commands)) : ?>
-<ul class="an-actions">
-<?php foreach($commands as $command) : ?>
-<li><?= $helper->command($command) ?></li> 
-<?php endforeach; ?>
-</ul>
-<?php endif;?>
+
+<?php if ( ! empty($commands) and ( ! ($commands instanceof Countable or is_array($commands)) or count($commands))): ?>
+    <ul class="an-actions">
+        <?php foreach ($commands as $command): ?>
+            <li><?= $helper->command($command) ?></li>
+        <?php endforeach ?>
+    </ul>
+<?php endif ?>
