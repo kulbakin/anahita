5317264a627cf6dda3292a12ccb189d3d709f7eb
Pavel Kulbakin <p.kulbakin@gmail.com>
2014-08-15 21:40:39 +0400

    fix: remove unnecessary iframe form submittion for avatar uploading

--- src/site/components/com_actors/views/settings/html/avatar.php
+++ src/site/components/com_actors/views/settings/html/avatar.php
@@ -2,20 +2,18 @@
 
 <h3><?= @text('COM-ACTORS-PROFILE-EDIT-AVATAR') ?></h3>
 
-<form id="actor-avatar" action="<?= @route($item->getURL().'&edit=avatar') ?>" method="post" target="hidden" enctype="multipart/form-data">
+<form id="actor-avatar" action="<?= @route($item->getURL().'&edit=avatar') ?>" method="post" enctype="multipart/form-data">
     <p><?= @avatar($item, 'medium') ?></p>
-    
     <p><?= @text('LIB-AN-AVATAR-SELECT-IMAGE-ON-YOUR-COMPUTER') ?></p>
     
     <div class="control-group">
         <div class="controls">
-            <input class="input-file" type="file" name="portrait" onchange="this.form.spin(); this.form.submit(); window.refresh=true" />
+            <input class="input-file" type="file" name="portrait" onchange="this.form.spin(); this.form.submit();" />
         </div>
     </div>
     <?php if ($item->portraitSet()): ?>
         <div class="form-actions">
-            <button onclick="this.form.spin();this.form.submit();window.refresh=true" class="btn btn-danger"><?= @text('LIB-AN-AVATAR-REMOVE-AVATAR') ?></button>
+            <button onclick="this.form.spin(); this.form.submit();" class="btn btn-danger"><?= @text('LIB-AN-AVATAR-REMOVE-AVATAR') ?></button>
         </div>
     <?php endif ?>
-    <iframe name="hidden" class="hide" onload="if( window.refresh) document.location.reload();"></iframe>
 </form>
